332ca37423bada0e81b8bd6722edda9db2bdc0ff
Stefano Moioli <smxdev4@gmail.com>
2024-03-17 04:58:12 +0100

    add php version of process functions

---
+++ pdb.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @author Stefano Moioli <smxdev4@gmail.com>
+ */
+
+namespace Pdb;
+
+class Concat {
+	private $blocks;
+	public function __construct($items){
+		$this->blocks = array_map(fn($x) => $x->data(), $items);
+	}
+	public function decode($src){
+		return implode('', $this->blocks);
+	}
+}
+
+class ConcatPages {
+	private $blocks;
+	public function __construct($items){
+		$this->blocks = array_map(fn($x) => $x->page(), $items);
+	}
+	public function decode($src){
+		return (new Concat($this->blocks))->decode($src);
+	}
+}
+
+class Cat {
+	private $item;
+	public function __construct($item){
+		$this->item = $item;
+	}
+	public function decode($src){
+		return $this->item;
+	}
+}
