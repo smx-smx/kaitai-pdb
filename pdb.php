1528971b2dde93a079a54b9d9bd653b947d5f4ee
Stefano Moioli <smxdev4@gmail.com>
2024-03-18 01:59:55 +0100

    pdb.php: duplicate "Cat" into the root namespace to fix class not found exception this might be a possible Kaitai bug. it happens since `tpi` and `dbi` have been moved to the top level declaration (see commit 881d916b1fd8fd8af2669dbe21d4097e35b07309)

--- pdb.php
+++ pdb.php
@@ -3,34 +3,46 @@
  * @author Stefano Moioli <smxdev4@gmail.com>
  */
 
-namespace Pdb;
-
-class Concat {
-	private $blocks;
-	public function __construct($items){
-		$this->blocks = array_map(fn($x) => $x->data(), $items);
+namespace Pdb {
+	class Concat {
+		private $blocks;
+		public function __construct($items){
+			$this->blocks = array_map(fn($x) => $x->data(), $items);
+		}
+		public function decode($src){
+			return implode('', $this->blocks);
+		}
 	}
-	public function decode($src){
-		return implode('', $this->blocks);
+
+	class ConcatPages {
+		private $blocks;
+		public function __construct($items){
+			$this->blocks = array_map(fn($x) => $x->page(), $items);
+		}
+		public function decode($src){
+			return (new Concat($this->blocks))->decode($src);
+		}
+	}
+
+	class Cat {
+		private $item;
+		public function __construct($item){
+			$this->item = $item;
+		}
+		public function decode($src){
+			return $this->item;
+		}
 	}
 }
 
-class ConcatPages {
-	private $blocks;
-	public function __construct($items){
-		$this->blocks = array_map(fn($x) => $x->page(), $items);
-	}
-	public function decode($src){
-		return (new Concat($this->blocks))->decode($src);
-	}
-}
-
-class Cat {
-	private $item;
-	public function __construct($item){
-		$this->item = $item;
-	}
-	public function decode($src){
-		return $this->item;
+namespace {
+	class Cat {
+		private $item;
+		public function __construct($item){
+			$this->item = $item;
+		}
+		public function decode($src){
+			return $this->item;
+		}
 	}
 }
